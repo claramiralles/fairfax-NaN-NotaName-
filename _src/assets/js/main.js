79cdda2636db69821e1caaec3ee0a4a7af3f382c
anniavdc <anva1189@gmail.com>
2019-04-03 23:27:55 +0200

    Add type function Github

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const formGithub = document.getElementById('github');
+const previewGithub = document.querySelector('.github');
+
+function writeGithub(event){
+    console.log('Hola');
+    const textGithub = event.currentTarget;
+
+    previewGithub.href = textGithub.value;
+}
+
+formGithub.addEventListener('keyup', writeGithub);
