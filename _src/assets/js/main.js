ef6c69b2f03e9b1ef4033b0feca6539badb6d380
claramiralles <clara.miralles.imperial@gmail.com>
2019-04-03 20:46:57 +0200

    function handle-input-user-name-change

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,16 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputUserName = document.getElementById('user-name');
+const userName = document.querySelector('.name');
+
+function handleInputUserNameChange(){
+    const name = inputUserName.value;
+    
+    if (name === ''){
+        userName.innerHTML=`Nombre Apellido`;
+    }else{
+        userName.innerHTML=name;
+    }
+    console.log(`Funciona`);
+}
+inputUserName.addEventListener('keyup', handleInputUserNameChange);
