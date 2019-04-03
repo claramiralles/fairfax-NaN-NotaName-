ea51babf45ac02b36c8a54c3442b0b7df799b72a
libertadpozos <libertad.pozos@gmail.com>
2019-04-03 23:42:03 +0200

    Add JS to name and position of the form

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,21 @@
 'use strict';
 
-console.log('>> Ready :)');
+const namePreview=document.querySelector('.name');
+const nameInput=document.querySelector('#name');
+const jobPreview=document.querySelector('.job');
+const jobInput=document.querySelector('#position');
+
+
+function writeName(event){
+    namePreview.innerHTML=nameInput.value || "Nombre Apellido";
+}
+nameInput.addEventListener('keyup', writeName);
+
+function writePosition(event){
+    jobPreview.innerHTML=jobInput.value || "Front-end developer";
+}
+jobInput.addEventListener('keyup', writePosition);
+
+
+
+
