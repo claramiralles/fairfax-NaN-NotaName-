c39de65c7add63cd053aeab41be2c03c31894708
sandrapinazo <sandrapinazogonzalez@gmail.com>
2019-04-04 15:51:32 +0200

    Add mail preview

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,18 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+const inputMail=document.querySelector(".input-mail");
+
+function writeMail() {
+    const yourEmail=inputMail.value;
+    const previewMail=document.querySelector(".buttonMail");    
+    if (!yourEmail){
+        previewMail.classList.add("hide");
+    }else{
+        previewMail.classList.remove("hide");
+        previewMail.href=`mailto:${yourEmail}`;
+    }
+}
+inputMail.addEventListener("keyup",writeMail);
+
