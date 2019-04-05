f39d7b6e2f5e2ad6d0fb52a610eb62f51bd6fa51
claramiralles <clara.miralles.imperial@gmail.com>
2019-04-05 07:43:28 +0200

    form-fill id adapted

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,7 @@
 'use strict';
 
 console.log('>> Ready :)');
+//Linkedin input
+
+const inputLinkedin=document.querySelector('.Linkedin');
+const previewLinkedin=document.getElementById('Linkedin');
