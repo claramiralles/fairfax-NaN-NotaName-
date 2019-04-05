cb7550595e063ea61661227588023f19818a9b71
claramiralles <clara.miralles.imperial@gmail.com>
2019-04-05 08:42:50 +0200

    function details improved

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,11 +1,12 @@
 'use strict';
 
 //Linkedin
-const inputLinkedin=document.getElementById('Linkedin');
-const previewLinkedin=document.querySelector('.linkedin');
+const inputLinkedin = document.getElementById('Linkedin');
+const previewLinkedin = document.querySelector('.linkedin');
 
 function writeLinkedin(event){
-    const textLinkedin=event.currentTarget;
-    previewLinkedin.href=textLinkedin.value;
+    const textLinkedin = event.currentTarget;
+
+    previewLinkedin.href = textLinkedin.value;
 }
 inputLinkedin.addEventListener('keyup', writeLinkedin);
