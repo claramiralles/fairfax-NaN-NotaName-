131eaa1ee8c051b7e6c7e8f8acb6a96d1e3788c5
claramiralles <clara.miralles.imperial@gmail.com>
2019-04-05 08:05:13 +0200

    initial inputtopreview function

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -7,3 +7,16 @@ console.log('>> Ready :)');
 const inputLinkedin=document.querySelector('.linkedin');
 //declara el input
 const previewLinkedin=document.getElementById('Linkedin');
+
+//declaro la funcion handler -> cuando el value de input tenga texto, quiero que el innerHTML del preview sea ese value 
+//cuando el value de input no tenga texto, quiero que el innerHTML del preview este vacio.
+
+const putInputValueInPreview = function () {
+if(inputLinkedin===previewLinkedin){
+    previewLinkedin.href=inputLinkedin.innerHTML;
+} else if (inputLinkedin===''){
+    previewLinkedin='';
+}
+}
+//declaro el event listener. quiero escuchar cuando input tenga texto
+previewLinkedin.addEventListener('keyup', putInputValueInPreview);
