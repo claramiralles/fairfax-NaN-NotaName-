f5fcd9007c5488d7ff508c9a2458961379fe45d5
claramiralles <clara.miralles.imperial@gmail.com>
2019-04-05 08:26:02 +0200

    function ok

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,22 +1,11 @@
 'use strict';
 
-console.log('>> Ready :)');
-//Linkedin input
+//Linkedin
+const inputLinkedin=document.getElementById('Linkedin');
+const previewLinkedin=document.querySelector('.linkedin');
 
-//declara el card preview
-const inputLinkedin=document.querySelector('.linkedin');
-//declara el input
-const previewLinkedin=document.getElementById('Linkedin');
-
-//declaro la funcion handler -> cuando el value de input tenga texto, quiero que el innerHTML del preview sea ese value 
-//cuando el value de input no tenga texto, quiero que el innerHTML del preview este vacio.
-
-const putInputValueInPreview = function () {
-if(inputLinkedin===previewLinkedin){
-    previewLinkedin.href=inputLinkedin.innerHTML;
-} else if (inputLinkedin===''){
-    previewLinkedin='';
+function writeLinkedin(event){
+    const textLinkedin=event.currentTarget;
+    previewLinkedin.href=textLinkedin.value;
 }
-}
-//declaro el event listener. quiero escuchar cuando input tenga texto
-previewLinkedin.addEventListener('keyup', putInputValueInPreview);
+inputLinkedin.addEventListener('keyup', writeLinkedin);
