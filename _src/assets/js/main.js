1ce8f5b9ffce386ae10269fae88572ce1dcb2be5
Elvira Fuente <hebebicho@gmail.com>
2019-04-21 20:05:57 +0200

    change default image route

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -281,7 +281,7 @@ function reset() {
   cardContainerEl.classList.remove('theme--red');
   cardContainerEl.classList.add('theme--green');
   //default img
-  profileImage.style.backgroundImage = "url('../../assets/images/default-avatar.jpg')";
+  profileImage.style.backgroundImage = "url('../images/default-avatar.jpg')";
   profilePreview.style.backgroundImage = "url('#')";
 
   resetCache();
